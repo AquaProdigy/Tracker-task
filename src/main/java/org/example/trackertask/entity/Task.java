3325c58d5de3d2efab3aa404e228375039b490d7
daniil <suprychev228338@gmail.com>
2026-05-12 12:50:14 +0300

    fixed NoargsConstructor

--- src/main/java/org/example/trackertask/entity/Task.java
+++ src/main/java/org/example/trackertask/entity/Task.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
         uniqueConstraints = @UniqueConstraint(name = "uq_tasks_user_title", columnNames = {"user_id", "title"}))
 @Getter
 @Setter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
